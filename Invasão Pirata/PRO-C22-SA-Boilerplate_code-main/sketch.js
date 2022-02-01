b5c4cfa83b6c23f650056585050c623a11135c71
MariaRaya <94574813+MariaRaya@users.noreply.github.com>
2022-02-01 20:05:33 -0300

    Add files via upload

--- Invasão Pirata/PRO-C22-SA-Boilerplate_code-main/sketch.js
+++ Invasão Pirata/PRO-C22-SA-Boilerplate_code-main/sketch.js
@@ -1,3 +1,4 @@
+// Renomeando módulos da biblioteca
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -6,11 +7,12 @@ var engine, world, backgroundImg;
 
 var canvas, angle, tower, ground;
 var cannon, cannonBall;
-var boat;
+var boat; 
 
 var bolas = [];
+var Boats = [];
 
-
+//Carregando imagens e animações
 function preload() {
   backgroundImg = loadImage("./imagem/background.gif");
   towerImage = loadImage("./imagem/tower.png");
@@ -19,13 +21,17 @@ function preload() {
 function setup() {
 
   canvas = createCanvas(1200, 600);
+
+  //Criando um motor e adicionando a um mundo
   engine = Engine.create();
   world = engine.world;
   
+  //Mantendo o objeto parado
   var options = {
     isStatic: true
   }
 
+  //Criando corpo e adicionando ao mundo
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
   World.add(world, ground);
 
@@ -37,19 +43,19 @@ function setup() {
   angle = 20;
   cannon = new Cannon(180, 110, 130, 100, angle);
   cannonBall = new CannonBall(cannon.x, cannon.y);
-  boat = new Boat(width, height - 60, 170, 170, -80);
 
 
 }
 
 function draw() {
   image(backgroundImg,0,0,1200,600)
+  //Atualizando as realidades físicas 
   Engine.update(engine);
 
-  
+  //Desenhando na tela
   rect(ground.position.x, ground.position.y, width * 2, 1);
   
-
+  //Dsenhando torre na tela
   push();
   imageMode(CENTER);
   image(towerImage,tower.position.x, tower.position.y, 160, 310);
@@ -57,19 +63,16 @@ function draw() {
 
   // mostrando canhão na tela
   cannon.display();
-  boat.display();
+  mostrarBarco();
 
-  for(i = 0; i < bolas.length; i++){
+  //Loop para mostrar as bolas na tela
+  for(var i = 0; i < bolas.length; i++){
     mostrarCannonBalls(bolas[i],i);
+    colisaoBarco(i);
   }
 }
 
-function keyReleased(){
-  if(keyCode == DOWN_ARROW){
-    bolas[bolas.length - 1].atirar();
-  }
-}
-
+// Se apertar o botão, a bola será criada;
 function keyPressed(){
   if(keyCode == DOWN_ARROW){
     cannonBall = new CannonBall(cannon.x, cannon.y);
@@ -77,8 +80,51 @@ function keyPressed(){
   }
 }
 
+// Função display
 function mostrarCannonBalls(bola,i){
     if(bola){
       bola.display();
     }
+}
+
+//Se apertar o botão, a bola dentro da matriz será atirada
+function keyReleased(){
+  if(keyCode == DOWN_ARROW){
+    bolas[bolas.length - 1].atirar();
+  }
+}
+
+function mostrarBarco(){
+  if(Boats.length > 0){
+    if(Boats[Boats.length - 1].body.position.x < width - 300 || Boats[Boats.length - 1] == undefined){
+      boat = new Boat(width - 80, height - 60, 170, 170, -80);
+      Boats.push(boat);
+    }
+
+    for(var i = 0; i < Boats.length; i++){
+      if(Boats[i]){
+        	Matter.Body.setVelocity(Boats[i].body,{x:-1,y:0});
+          Boats[i].display();
+      }
+    }
+
+  } else{
+    boat = new Boat(width - 80, height - 60, 170, 170, -80);
+    Boats.push(boat);
+  }
+}
+
+function colisaoBarco (index){
+  for (var i=0; i < Boats.length; i++){
+    if(bolas[index] !== undefined && Boats[i] !== undefined){
+
+      var collision = Matter.SAT.collides(bolas[index].body, Boats[i].body);
+
+      if(collision.collided){
+        Boats[i].remove(i);
+        bolas[i].remove(i);
+      }
+
+    }
+  }
 }
